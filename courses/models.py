9afc60c29b054b6744adf839e9dc7fee744410f7
anton <a.makushchenko@gmail.com>
2016-07-12 17:17:50 +0300

    added diverse content models - file text video image

--- courses/models.py
+++ courses/models.py
@@ -49,9 +49,45 @@ class Module(models.Model):
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name="contents")
     # связь с моделью contentType
-    content_type = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(ContentType,
+                                     limit_choices_to={
+                                        'model__in': ('file',
+                                                      'image',
+                                                      'text',
+                                                      'video')
+                                     })
     # сохранение PK связанного объекта
     object_id = models.PositiveIntegerField()
     # определение обобщенной связи
     content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class BaseContent(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name="%(class)s_related")
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class File(BaseContent):
+    file = models.FileField(upload_to="files")
+
+
+class Image(BaseContent):
+    image = models.FileField(upload_to="images")
+
+
+class Text(BaseContent):
+    text = models.TextField()
+
+
+class Video(BaseContent):
+    url = models.URLField()
 
