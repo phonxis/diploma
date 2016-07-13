6620729166d5594c81bf182a9a30f5b700753994
anton <a.makushchenko@gmail.com>
2016-07-13 13:26:12 +0300

    added Order field

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -41,9 +42,14 @@ class Module(models.Model):
                                related_name="modules")
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True,
+                       for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return "{}. {}".format(self.order, self.title)
 
 
 class Content(models.Model):
@@ -60,6 +66,11 @@ class Content(models.Model):
     object_id = models.PositiveIntegerField()
     # определение обобщенной связи
     content_object = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True,
+                       for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class BaseContent(models.Model):
