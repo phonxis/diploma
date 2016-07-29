6901fe4ee6f66888640d52fb75f6faf4b09d5df1
anton <phonxis@gmail.com>
2016-07-29 18:40:29 +0300

    some templates have been styling with bootstrap
    login, registration and base templates have been styling with bootstrap

--- students/forms.py
+++ students/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from courses.models import Course
 
@@ -13,40 +14,62 @@ class CourseEnrollForm(forms.Form):
 class UsersLoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Username'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Username',
+                   'required': 'True'}
         )
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Password'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Password',
+                   'required': 'True'}
         )
     )
 
 
 class UsersCreationForm(UserCreationForm):
-    username = forms.CharField(
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+        widgets = {
+            'username': forms.TextInput(
+                    attrs={'class': 'form-control form-group',
+                           'placeholder': 'Username',
+                           'required': 'True'}
+            ),
+            'email': forms.TextInput(
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Email address',
+                   'required': 'True'}
+            )
+        }
+
+    '''username = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Username'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Username',
+                   'required': 'True'}
         )
     )
     email = forms.EmailField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Email address'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Email address',
+                   'required': 'True'}
         )
-    )
+    )'''
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Password'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Password',
+                   'required': 'True'}
         )
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Confirm password'}
+            attrs={'class': 'form-control form-group',
+                   'placeholder': 'Confirm password',
+                   'required': 'True'}
         )
     )
