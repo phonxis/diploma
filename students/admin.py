2623a19f03e2dbb9c5c9080e9b77e06d8b9e890d
phonxis <phonxis@gmail.com>
2017-01-08 11:23:01 +0200

    added Inline form to User model

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+admin.site.unregister(User)
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    inlines = [ProfileInline,]
+
+admin.site.register(User, UserAdmin)
