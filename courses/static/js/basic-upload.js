da112f27437d672407405674008ea609229f1779
phonxis <phonxis@gmail.com>
2017-01-11 15:07:42 +0200

    added ajax file uploading

--- courses/static/js/basic-upload.js
+++ courses/static/js/basic-upload.js
@@ -1,15 +1,76 @@
 $(function () {
-  /* 1. OPEN THE FILE EXPLORER WINDOW */
+  /*
+
   $(".js-upload-photos").click(function () {
     $("#fileupload").click();
   });
 
-  /* 2. INITIALIZE THE FILE UPLOAD COMPONENT */
+
   $("#fileupload").fileupload({
     dataType: 'json',
-    done: function (e, data) {  /* 3. PROCESS THE RESPONSE FROM THE SERVER */
+    paramName: 'data_field',
+    done: function (e, data) {
       console.log(data.result)
     }
-  });
+  });*/
 
+uploadButton = $('<button/>')
+            .addClass('btn btn-primary')
+            .prop('disabled', true)
+            .text('Processing...')
+            .on('click', function () {
+                var $this = $(this),
+                    data = $this.data();
+                $this
+                    .off('click')
+                    .text('Uploading...')
+                    .on('click', function () {
+                        $this.remove();
+                        data.abort();
+                    });
+                data.submit().always(function () {
+                    $this.remove();
+                });
+            });
+
+  $('#fileupload').fileupload({
+        dataType: 'json',
+        autoUpload: false,
+        paramName: 'data_field',
+        done: function (e, data) {
+          console.log(data.result)
+        }
+    }).on('fileuploadadd', function (e, data) {
+        data.context = $('<div/>').appendTo('#files');
+        $.each(data.files, function (index, file) {
+            var node = $('<p/>')
+                    .append($('<span/>').text(file.name));
+            if (!index) {
+                node
+                    .append('<br>')
+                    .append(uploadButton.clone(true).data(data));
+            }
+            node.appendTo(data.context);
+        });
+    }).on('fileuploadprocessalways', function (e, data) {
+        var index = data.index,
+            file = data.files[index],
+            node = $(data.context.children()[index]);
+        if (file.preview) {
+            node
+                .prepend('<br>')
+                .prepend(file.preview);
+        }
+        if (file.error) {
+            node
+                .append('<br>')
+                .append($('<span class="text-danger"/>').text(file.error));
+        }
+        if (index + 1 === data.files.length) {
+            data.context.find('button')
+                .text('Upload')
+                .prop('disabled', !!data.files.error);
+        }
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
 });
