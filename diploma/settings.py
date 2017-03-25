9deba1f3fff6eee4f4321e6e93e159bc0e8b4aaf
phonxis <phonxis@gmail.com>
2017-03-25 21:41:08 +0200

    changed staticfiles settings

--- diploma/settings.py
+++ diploma/settings.py
@@ -16,6 +16,7 @@ from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -185,6 +186,11 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # media
 MEDIA_URL = '/media/'
